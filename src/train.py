3faff1b37993b5a785cc56df581fc3062b80a470
SelimFEUP <84468279+SelimFEUP@users.noreply.github.com>
2025-06-14 10:18:15 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -4,3 +4,13 @@ import pandas as pd
 from src.model import SMAAE
 from src.data_loader import load_data
 
+def load_model():
+    model = SMAAE(input_dim=153, latent_dim=32, mem_dim=100)
+    model.compile(ae_optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), d_optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4))
+
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='reconstruction_loss', patience=7, mode='min')
+    mc = tf.keras.callbacks.ModelCheckpoint('models/model.keras', monitor='reconstruction_loss', verbose=7, save_best_only=True,mode='min')
+    
+    history = model.fit(data, epochs=100, batch_size=32, verbose=1, callbacks=[mc, early_stopping])
+    return history
+
